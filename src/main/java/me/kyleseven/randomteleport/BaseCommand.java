4a2c2056b444a5517588c9c1eab199f55f7466f7
kyleseven <kyle.senerpida@icloud.com>
2020-01-03 15:54:29 -0600

    Implement reload and help subcommands.

--- src/main/java/me/kyleseven/randomteleport/BaseCommand.java
+++ src/main/java/me/kyleseven/randomteleport/BaseCommand.java
@@ -23,76 +23,99 @@ public class BaseCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            if (player.hasPermission("randomteleport.teleport")) {
+            if (args.length == 0) {
+                if (player.hasPermission("randomteleport.teleport")) {
 
-                Location locFrom = player.getLocation();
-                Random randomGen = new Random();
+                    Location locFrom = player.getLocation();
+                    Random randomGen = new Random();
 
-                //If player has passed cooldown check OR has bypass permission
-                if (CooldownManager.checkCooldown(player) || player.hasPermission("randomteleport.bypass")) {
+                    //If player has passed cooldown check OR has bypass permission
+                    if (CooldownManager.checkCooldown(player) || player.hasPermission("randomteleport.bypass")) {
 
-                    //Get max and min from config
-                    double minRange = main.getConfig().getDouble("minimum_range"); // Minimum blocks the player is teleported away
-                    double maxRange = main.getConfig().getDouble("maximum_range"); // Maximum blocks the player is teleported away
+                        //Get max and min from config
+                        double minRange = main.getConfig().getDouble("minimum_range"); // Minimum blocks the player is teleported away
+                        double maxRange = main.getConfig().getDouble("maximum_range"); // Maximum blocks the player is teleported away
 
-                    //Difference in X coordinate the player will be teleported away
-                    double diffX = randomGen.nextDouble() * (maxRange - minRange) + minRange;
-                    double diffZ = randomGen.nextDouble() * (maxRange - minRange) + minRange;
+                        //Difference in X coordinate the player will be teleported away
+                        double diffX = randomGen.nextDouble() * (maxRange - minRange) + minRange;
+                        double diffZ = randomGen.nextDouble() * (maxRange - minRange) + minRange;
 
-                    //Old X Y Z coordinates
-                    double oldX = locFrom.getX(), oldY = locFrom.getY(), oldZ = locFrom.getZ();
+                        //Old X Y Z coordinates
+                        double oldX = locFrom.getX(), oldY = locFrom.getY(), oldZ = locFrom.getZ();
 
-                    //New X Y Z coordinates
-                    double newX, newY, newZ;
+                        //New X Y Z coordinates
+                        double newX, newY, newZ;
 
-                    //Randomly go in positive direction or negative direction
-                    if (randomGen.nextBoolean()) {
-                        newX = oldX + diffX;
+                        //Randomly go in positive direction or negative direction
+                        if (randomGen.nextBoolean()) {
+                            newX = oldX + diffX;
+                        } else {
+                            newX = oldX - diffX;
+                        }
+
+                        if (randomGen.nextBoolean()) {
+                            newZ = oldZ + diffZ;
+                        } else {
+                            newZ = oldZ - diffZ;
+                        }
+
+                        //Get a safe y coordinate
+                        //TODO: Find if player's new coordinate is over lava
+                        newY = player.getWorld().getHighestBlockAt((int) newX, (int) newZ).getY();
+
+                        //Save numbers to new coordinate
+                        Location locTo = new Location(player.getWorld(), newX, newY, newZ);
+
+                        //Teleport player to new coordinate
+                        player.teleport(locTo);
+
+                        //Calculate Distance between coordinate points
+                        double distance = Math.abs(locFrom.distance(locTo));
+
+                        //Send message saying the distance teleported and replace %METERS% with distance.
+                        String message = main.getConfig().getString("notification_msg");
+                        message = message.replaceAll("%METERS%", Integer.toString((int) Math.round(distance)));
+                        message = main.getConfig().getString("prefix_msg") + message;
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+
+                        //Add them to the cooldown if they don't have the bypass permission
+                        if (!player.hasPermission("randomteleport.bypass")) {
+                            CooldownManager.setCooldown(player, main.getConfig().getInt("cooldown_timer"));
+                        }
                     } else {
-                        newX = oldX - diffX;
-                    }
-
-                    if (randomGen.nextBoolean()) {
-                        newZ = oldZ + diffZ;
-                    } else {
-                        newZ = oldZ - diffZ;
-                    }
-
-                    //Get a safe y coordinate
-                    //TODO: Find if player's new coordinate is over lava
-                    newY = player.getWorld().getHighestBlockAt((int) newX, (int) newZ).getY();
-
-                    //Save numbers to new coordinate
-                    Location locTo = new Location(player.getWorld(), newX, newY, newZ);
-
-                    //Teleport player to new coordinate
-                    player.teleport(locTo);
-
-                    //Calculate Distance between coordinate points
-                    double distance = Math.abs(locFrom.distance(locTo));
-
-                    //Send message saying the distance teleported and replace %METERS% with distance.
-                    String message = main.getConfig().getString("notification_msg");
-                    message = message.replaceAll("%METERS%", Integer.toString((int) Math.round(distance)));
-                    message = main.getConfig().getString("prefix_msg") + message;
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-
-                    //Add them to the cooldown if they don't have the bypass permission
-                    if (!player.hasPermission("randomteleport.bypass")) {
-                        CooldownManager.setCooldown(player, main.getConfig().getInt("cooldown_timer"));
+                        //Send cooldown message and replace %SECONDS% with seconds left on cooldown
+                        int timeLeft = CooldownManager.getCooldown(player);
+                        String cdMessage = main.getConfig().getString("cooldown_msg");
+                        cdMessage = cdMessage.replaceAll("%SECONDS%", Integer.toString(timeLeft));
+                        cdMessage = main.getConfig().getString("prefix_msg") + cdMessage;
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', cdMessage));
                     }
                 } else {
-                    //Send cooldown message and replace %SECONDS% with seconds left on cooldown
-                    int timeLeft = CooldownManager.getCooldown(player);
-                    String cdMessage = main.getConfig().getString("cooldown_msg");
-                    cdMessage = cdMessage.replaceAll("%SECONDS%", Integer.toString(timeLeft));
-                    cdMessage = main.getConfig().getString("prefix_msg") + cdMessage;
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', cdMessage));
+                    // Return permission check fail message
+                    player.sendMessage(command.getPermissionMessage());
+                }
+            }
+            else if (args[0].equalsIgnoreCase("reload")) {
+                // Reload config
+                String message = main.getConfig().getString("reload_msg");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+                main.reloadConfig();
+            }
+            else if (args[0].equalsIgnoreCase("help")) {
+                // Show command message
+                String[] help = {"&8--------======= &cRTP Help &8=======--------",
+                                 "&c/rtp &8- &7Teleports to a random location.",
+                                 "&c/rtp reload &8- &7Reload the plugin config.",
+                                 "&c/rtp help &8- &7Show this help message."};
+
+                for (String s: help) {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', s));
                 }
             }
             else {
-                // Return permission check fail message
-                player.sendMessage(command.getPermissionMessage());
+                // Show invalid subcommand message
+                String message = main.getConfig().getString("invalid_msg");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', main.getConfig().getString("prefix_msg") + message));
             }
         }
         else {
