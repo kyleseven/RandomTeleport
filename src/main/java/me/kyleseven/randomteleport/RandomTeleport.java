1ad67d0739f13c98dda67ff7b8b51449c94b6118
kyleseven <kyle.senerpida@icloud.com>
2020-04-09 22:47:21 -0500

    Add new MainCommand to use with ACF

--- src/main/java/me/kyleseven/randomteleport/RandomTeleport.java
+++ src/main/java/me/kyleseven/randomteleport/RandomTeleport.java
@@ -1,22 +1,33 @@
 package me.kyleseven.randomteleport;
 
-import me.kyleseven.randomteleport.commands.BaseCommand;
+import co.aikar.commands.PaperCommandManager;
+import me.kyleseven.randomteleport.commands.MainCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
 
 public final class RandomTeleport extends JavaPlugin {
 
+    private static RandomTeleport plugin;
+    private static PaperCommandManager commandManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        registerCommands();
         CooldownManager.setupCooldown();
         this.saveDefaultConfig();
-        Objects.requireNonNull(this.getCommand("randomteleport")).setExecutor(new BaseCommand(this));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static RandomTeleport getPlugin() {
+        return plugin;
+    }
+
+    private void registerCommands() {
+        commandManager = new PaperCommandManager(this);
+        commandManager.registerCommand(new MainCommand());
     }
 }
---
+++ src/main/java/me/kyleseven/randomteleport/commands/MainCommand.java
@@ -0,0 +1,7 @@
+package me.kyleseven.randomteleport.commands;
+
+import co.aikar.commands.BaseCommand;
+
+public class MainCommand extends BaseCommand {
+
+}
