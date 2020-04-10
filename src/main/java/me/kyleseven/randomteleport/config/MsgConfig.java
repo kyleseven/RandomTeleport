0eca0d584295afd55f5b474856c033fd840244f2
kyleseven <kyle.senerpida@icloud.com>
2020-04-09 23:09:12 -0500

    Added MsgConfig class to handle messages.yml

---
+++ src/main/java/me/kyleseven/randomteleport/config/MsgConfig.java
@@ -0,0 +1,45 @@
+package me.kyleseven.randomteleport.config;
+
+public class MsgConfig extends ConfigLoader {
+    private static MsgConfig msgConfig;
+
+    public MsgConfig() {
+        super("config.yml");
+    }
+
+    public static MsgConfig getInstance() {
+        if (msgConfig == null) {
+            msgConfig = new MsgConfig();
+        }
+        return msgConfig;
+    }
+
+    public static void reload() {
+        msgConfig = null;
+        getInstance();
+    }
+
+    /*
+    Config keys
+     */
+
+    public String getPrefix() {
+        return config.getString("prefix");
+    }
+
+    public String getNotification(double meters) {
+        return config.getString("notification").replaceAll("%METERS", String.valueOf(meters));
+    }
+
+    public String getCooldown(int seconds) {
+        return config.getString("cooldown").replaceAll("%SECONDS", String.valueOf(seconds));
+    }
+
+    public String getReload() {
+        return config.getString("reload");
+    }
+
+    public String getInvalid() {
+        return config.getString("invalid");
+    }
+}
